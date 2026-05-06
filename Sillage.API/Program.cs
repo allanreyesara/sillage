415b361c10622e5d270d71ba1ef7b9823a354599
allanreyesara <a11anr3y3s@gmail.com>
2026-05-06 00:35:51 -0600

    feat(api): add production domain to CORS policy

--- Sillage.API/Program.cs
+++ Sillage.API/Program.cs
@@ -30,7 +30,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy( "AllowFrontend" ,policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173",
+                            "http://localhost:4173",
+                            "https://sillage.beauty",
+                            "https://www.sillage.beauty")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
